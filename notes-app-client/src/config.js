7908d984b47bf7dacac4ddf3f0cc3b591ea2ff0f
Corey Cotter <ccotter@splunk.com>
2018-12-12 15:36:33 -0800

    Added rest of react project

---
+++ notes-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-west-2",
+    BUCKET: "cotter-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-west-2",
+    URL: "https://odca5ehykl.execute-api.us-west-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_0jTajTxYo",
+    APP_CLIENT_ID: "696mi1mu601362h79krhsb1gd8",
+    IDENTITY_POOL_ID: "us-west-2:990b9756-e5f1-4a71-8950-e9eee3287b17"
+  }
+};
